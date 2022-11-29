f107990ef871a2b20c7f39ed03c35d083f615f36
Jake Chien <jake_chien@outlook.com>
2022-11-29 16:41:44 -0300

    feat: route / redirects to /login

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,18 +1,17 @@
 import React from 'react';
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { Redirect, Route, Switch } from 'react-router-dom';
 import './App.css';
 import DeliveryProvider from './context/DeliveryProvider';
 import LoginPage from './pages/Login';
 
 function App() {
   return (
-    <BrowserRouter>
-      <DeliveryProvider>
-        <Switch>
-          <Route path="/login" component={ LoginPage } />
-        </Switch>
-      </DeliveryProvider>
-    </BrowserRouter>
+    <DeliveryProvider>
+      <Switch>
+        <Redirect exact from="/" to="/login" />
+        <Route path="/login" component={ LoginPage } />
+      </Switch>
+    </DeliveryProvider>
   );
 }
 
