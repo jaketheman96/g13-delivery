543574543c777f8dbdbe1e4d23248828af9b0f07
Jake Chien <jake_chien@outlook.com>
2022-12-01 20:02:15 -0300

    fix: date format fixed

--- front-end/src/pages/Pedidos.js
+++ front-end/src/pages/Pedidos.js
@@ -4,6 +4,13 @@ import OrderCard from '../components/OrderCard';
 import salesMock from '../backend_mock/sales.mock';
 
 function Pedidos() {
+  const formatDate = (date) => {
+    const dataFormatada = `
+      ${date.getDate()}/${date.getMonth() + 1}/${date.getFullYear()}
+    `;
+    return dataFormatada;
+  };
+
   return (
     <>
       <Navbar />
@@ -14,7 +21,7 @@ function Pedidos() {
             id={ sale.id }
             price={ sale.total_price }
             status={ sale.status }
-            date={ sale.saleDate.toLocaleDateString() }
+            date={ formatDate(sale.saleDate) }
           />
         ))}
       </section>
