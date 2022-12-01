063c90dfe02c299146633fd99332ae67ffb66f03
Jake Chien <jake_chien@outlook.com>
2022-12-01 17:45:02 -0300

    feat: add orders page and implements handleclick on navbar

---
+++ front-end/src/pages/Pedidos.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Navbar from '../components/Navbar';
+
+function Pedidos() {
+  return (
+    <div>
+      <Navbar />
+    </div>
+  );
+}
+
+export default Pedidos;
