dd8edcc60ba3b46589fc4c04bb260b1958de642a
Jake Chien <jake_chien@outlook.com>
2022-12-07 20:23:34 -0300

    feat: add's link in orders to access order details

--- front-end/src/components/OrderCard.js
+++ front-end/src/components/OrderCard.js
@@ -1,32 +1,41 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
 
 function OrderCard({ id, price, date, status }) {
   const MINIMUN_ZEROS = 3;
 
   return (
-    <div style={ { display: 'flex', justifyContent: 'space-evenly' } }>
-      <h5
-        data-testid={ `customer_orders__element-order-id-${id}` }
-      >
-        {`Pedido: ${id.toString().padStart(MINIMUN_ZEROS, '0')}`}
-      </h5>
-      <h2
-        data-testid={ `customer_orders__element-delivery-status-${id}` }
-      >
-        {status}
-      </h2>
-      <h4
-        data-testid={ `customer_orders__element-order-date-${id}` }
-      >
-        {date}
-      </h4>
-      <h4
-        data-testid={ `customer_orders__element-card-price-${id}` }
-      >
-        {price.replace('.', ',')}
-      </h4>
-    </div>
+    <Link
+      to={ `/customer/orders/${id}` }
+      style={ {
+        textDecoration: 'none',
+        color: 'black',
+      } }
+    >
+      <div style={ { display: 'flex', justifyContent: 'space-evenly' } }>
+        <h5
+          data-testid={ `customer_orders__element-order-id-${id}` }
+        >
+          {`Pedido: ${id.toString().padStart(MINIMUN_ZEROS, '0')}`}
+        </h5>
+        <h2
+          data-testid={ `customer_orders__element-delivery-status-${id}` }
+        >
+          {status}
+        </h2>
+        <h4
+          data-testid={ `customer_orders__element-order-date-${id}` }
+        >
+          {date}
+        </h4>
+        <h4
+          data-testid={ `customer_orders__element-card-price-${id}` }
+        >
+          {price.replace('.', ',')}
+        </h4>
+      </div>
+    </Link>
   );
 }
 
