7891ac9773c6685a9998b5bb456568a6b1e32c66
GusCabraal <gustavocabral97@hotmail.com>
2022-12-13 19:41:26 -0300

    test: token de autenticação

--- back-end/src/tests/integration/mocks/utils.mock.js
+++ back-end/src/tests/integration/mocks/utils.mock.js
@@ -1,9 +1,11 @@
 const JWT_VALID_VERIFY_ADMIN = { data: { role: 'administrator', id: 1 } };
 const JWT_VALID_VERIFY_SELLER = { data: { role: 'seller', id: 1 } };
 const JWT_VALID_VERIFY_CUSTOMER = { data: { role: 'customer', id: 1 } };
+const JWT_INVALID_VERIFY = { data: null };
 
 module.exports = { 
     JWT_VALID_VERIFY_ADMIN,
     JWT_VALID_VERIFY_SELLER,
-    JWT_VALID_VERIFY_CUSTOMER
+    JWT_VALID_VERIFY_CUSTOMER,
+    JWT_INVALID_VERIFY
 }
